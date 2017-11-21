1aa825a43817675ad1c2c14e81a316499da502d8
denny9527 <denny9527@qq.com>
2017-11-21 14:57:44 +0800

    commit modify 002

--- TestSpringBoot/src/main/java/com/denny/TestSpringBootApplication.java
+++ TestSpringBoot/src/main/java/com/denny/TestSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestSpringBootApplication {
 
 	public static void main(String[] args) {
-		//test
+		//test-1
 		SpringApplication.run(TestSpringBootApplication.class, args);
 	}
 }
